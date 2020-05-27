10c2db13e0e8899d054bcd1dbd2c00bb9fe674d5
LimingTeng <tengliming96@gmail.com>
2020-05-26 22:14:03 -0700

    URL update

--- service/main.go
+++ service/main.go
@@ -32,7 +32,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://34.69.115.4:9200"
+	ES_URL = "http://35.188.83.175:9200"
 )
 
 func main() {
